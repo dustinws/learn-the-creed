53746d3be19112a2cae36117ea0caae5df37300d
Dustin <duwstiles@pm.me>
2022-08-19 13:47:56 +0200

    bug(assets): include pngs in vite build for integration with offline use

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
 import classNames from '../utils/classNames';
 import pkg from '../../package.json';
 
+import seargentPng from '../assets/seargent.png';
+import githubPng from '../assets/github.png';
+
 export default function Header() {
   return (
     <div
@@ -16,7 +19,7 @@ export default function Header() {
     >
       <div className="flex items-center">
         <a href="/">
-          <img className="h-8" src="/seargent.png" alt="" />
+          <img className="h-8" src={seargentPng} alt="" />
         </a>
         <div
           className={classNames(
@@ -33,7 +36,7 @@ export default function Header() {
       </div>
       <div>
         <a href="https://github.com/dustinws/learn-the-creed">
-          <img className="h-8" src="/github.png" alt="" />
+          <img className="h-8" src={githubPng} alt="" />
         </a>
       </div>
     </div>
