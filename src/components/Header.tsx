d3c44f73a7c900ee76569bb8aa84c3b47015439e
Dustin <duwstiles@pm.me>
2022-08-19 12:15:34 +0200

    feature(header): add link to github

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import classNames from '../utils/classNames';
+import Github from '../assets/github.svg';
 
 export default function Header() {
   return (
@@ -7,24 +8,33 @@ export default function Header() {
         'flex',
         'w-full',
         'items-center',
+        'justify-between',
         'h-16',
+        'px-4',
         'bg-indigo-100',
       )}
     >
-      <a href="/">
-        <img className="h-8 pl-4" src="/seargent.png" alt="" />
-      </a>
-      <div
-        className={classNames(
-          'text-4xl',
-          'pl-4',
-          'justify-self-center',
-        )}
-      >
-        Learn the Creed
+      <div className="flex items-center">
+        <a href="/">
+          <img className="h-8" src="/seargent.png" alt="" />
+        </a>
+        <div
+          className={classNames(
+            'text-4xl',
+            'pl-4',
+            'justify-self-center',
+          )}
+        >
+          Learn the Creed
+        </div>
+        <div className="font-thin px-4">
+          v0.0.4
+        </div>
       </div>
-      <div className="font-thin px-4">
-        v0.0.4
+      <div>
+        <a href="https://github.com/dustinws/learn-the-creed">
+          <img className="h-8" src="/github.png" alt="" />
+        </a>
       </div>
     </div>
   );
