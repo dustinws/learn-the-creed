6bce94deba0ef17920efb46fd353fb3e321b669a
Dustin Stiles <dustin.w.stiles2.mil@army.mil>
2023-10-13 19:29:12 -0400

    update build dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import { plugin as elmPlugin } from 'vite-plugin-elm'
 
-const isProd = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
   plugins: [
-    elmPlugin({
-      debug: !isProd,
-      optimize: isProd,
-    }),
+    elmPlugin(),
   ]
 });
